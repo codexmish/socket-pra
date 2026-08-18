b7058413ded1956ccc384190e90875848794a2eb
codexmish <majharulislam.shanto.70@gmail.com>
2026-08-18 14:54:16 +0600

    initial socked added

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 const app = express()
 
-app.use(express.json())
+// app.use(express.json())
+const server = createServer(app)
+const io = new Server(server,{
+    cors:{
+        origin: "http://localhost:5173"
+    }
+})
 
-app.listen(8000, ()=>{
-    console.log("runnng");
+let allTask = []
+
+io.on('connection',(socket)=>{
+    console.log("a user connected: ", socket.id);
+    socket.on("task",(value)=>{
+        // console.log(value);
+        allTask.push(value)
+        socket.emit("taskClient", allTask)
+    })
     
+    socket.on('disconnect', (dis)=>{
+        console.log("disconnected", socket.id);
+        
+    })
+})
+
+server.listen(8000, ()=>{
+    console.log("runnng  ");
 })
