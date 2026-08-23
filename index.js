bae887705d8ac606e4276e205e93e6ce77cc7bea
codexmish <majharulislam.shanto.70@gmail.com>
2026-08-23 13:16:11 +0600

    crud added

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const dotenv = require("dotenv").config()
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
 const dbConfig = require("./configs/dbConfig");
-const dns = require("dns")
+const dns = require("dns");
+const taskSchema = require("./model/taskSchema");
 const app = express()
 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
@@ -16,18 +17,109 @@ const io = new Server(server,{
     }
 })
 
+
+
+// // ----get prev task from db
+// app.get("/api/task", async(req,res)=>{
+//     try {
+//         const tasks = await taskSchema.find()
+
+
+//     } catch (error) {
+        
+//     }
+// })
+
+
+
+
+
 let allTask = []
 
-io.on('connection',(socket)=>{
+io.on('connection',async(socket)=>{
     // console.log("a user connected: ", socket.id);
-    socket.on("task",(value)=>{
-        // console.log(value);
-        allTask.push(value)
-        io.emit("taskClient", allTask)
+
+    // -----get initial task
+    socket.on("getTask", async()=>{
+        try {
+    const initialTasks = await Task.find();
+
+    allTask = initialTasks
+    socket.emit('taskClient', allTask);
+
+  } catch (error) {
+    socket.emit('Failed to fetch initial tasks');
+  }
     })
 
 
-    io.emit("taskClient", allTask)
+//   -----create task
+  socket.on('create_task', async (data) => {
+    try {
+        const {title, description} = data
+      const newTask = await taskSchema.create({
+        title,
+        description
+      });
+
+      allTask.push(newTask)
+      io.emit('taskClient', allTask);
+    } catch (error) {
+      socket.emit('Failed to create task');
+    }
+  });
+
+
+
+  // ----- Update Task
+  socket.on('update_task', async (data) => {
+    try {
+      const { id, title, description, completed } = data;
+      const updatedTask = await taskSchema.findByIdAndUpdate(
+        id,
+        { title, description, completed },
+        {returnDocument: "after"}
+      );
+      
+      if (updatedTask) {
+        const taskList = await taskSchema.find()
+        allTask = taskList
+        io.emit('taskClient', allTask);
+      }
+    } catch (error) {
+      socket.emit('Failed to update task');
+    }
+  });
+
+
+
+  // ----- Delete Task
+  socket.on('delete_task', async (id) => {
+    try {
+      await taskSchema.findByIdAndDelete(id);
+      const taskList = await taskSchema.find()
+        allTask = taskList
+        io.emit('taskClient', allTask);
+
+    } catch (error) {
+      socket.emit('Failed to delete task');
+    }
+  });
+
+
+
+
+
+
+
+    // socket.on("task",(value)=>{
+    //     // console.log(value);
+    //     allTask.push(value)
+    //     io.emit("taskClient", allTask)
+    // })
+
+
+    // io.emit("taskClient", allTask)
     
     socket.on('disconnect', (dis)=>{
         // console.log("disconnected", socket.id);
